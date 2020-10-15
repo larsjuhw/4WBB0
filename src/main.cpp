8839e0e4116eee19e8fb7349a6b6f61bbe8d7e8e
Lars Wolter <larske.w@gmail.com>
2020-10-15 13:37:26 +0200

    Various changes and fixes
    - Now uses short beeps instead of tone changes to indicate liquid volume
    - Fixed buttons not working
    - Added tare-pin to use when debugging
    - Added debug serial outputs
    - Fixed weight logic

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ const int buttons[3] = { BT1, BT2, BT3 };
 const int buttonWeights[3] = { WEIGHT1, WEIGHT2, WEIGHT3 };
 
 int activeButton = 0; // Initialize the active button as BT1
-int activeWeight = WEIGHT1;
+long activeWeight = WEIGHT1;
 
 bool toneEnabled = false;
 
@@ -28,6 +28,14 @@ void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
 
+    pinMode(buttons[0], INPUT_PULLUP);
+    pinMode(buttons[1], INPUT_PULLUP);
+    pinMode(buttons[2], INPUT_PULLUP);
+
+#ifdef DEBUG
+    pinMode(TARE_PIN, INPUT_PULLUP);
+#endif
+
     scale.begin(HX711_DT, HX711_SCK);
     calibrate();
 
@@ -39,32 +47,48 @@ void loop() {
     // First, check if a button is pressed
     for (int i = 0; i < 3; i++) {
         if (digitalRead(buttons[i]) == PRESSED) {
+#ifdef DEBUG
+            Serial.print("Detect press of button ");
+            Serial.println(i);
+#endif
             buttonHandler(i);
         }
     }
 
-    long raw_reading = scale.get_units(5);
+#ifdef DEBUG
+    if (digitalRead(TARE_PIN) == PRESSED) {
+        scale.tare();
+        Serial.println("Tare reset");
+    }
+#endif
+
+    long raw_reading = scale.get_units(4);
     long reading = (raw_reading > 0) ? raw_reading : 0;
-    float percentage = reading / activeWeight;
+    float percentage = (float) reading / activeWeight;
 
-    float tone_factor = (percentage <= 1) ? percentage : 1.0;
+#ifdef DEBUG
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(10);
+    digitalWrite(LED_BUILTIN, LOW);
+#endif
 
-    if (percentage > 0.6) {
-        // tone(BUZZER, 262 + tone_factor * 785);
-        toneEnabled = true;
-    } else if (toneEnabled) {
-        // noTone(BUZZER);
-        digitalWrite(BUZZER, HIGH);
-        toneEnabled = false;
+    if (percentage > PERCENTAGE1) {
+        beep(50, 50, 3);
+    } else if (percentage > PERCENTAGE2) {
+        beep(50, 50, 2);
+    } else if (percentage > PERCENTAGE3) {
+        beep(50, 50, 1);
     }
 
 #ifdef DEBUG
-    Serial.print("Reading: ");
+    Serial.print("raw: ");
     Serial.print(raw_reading);
+    Serial.print(" - val: ");
+    Serial.print(reading);
     Serial.print(" - percentage: ");
     Serial.print(percentage);
-    Serial.print(" - tone: ");
-    Serial.println(262 + tone_factor * 785);
+    Serial.print(" - activeWeight: ");
+    Serial.println(activeWeight);
 #endif
 }
 
@@ -92,7 +116,7 @@ void calibrate() {
 void ready() {
     scale.tare();
 
-    beep(100, 50, 2);
+    beep(50, 50, 2);
 
 #ifdef DEBUG
     Serial.println("Setup finished");
@@ -107,15 +131,14 @@ void buttonHandler(int index) {
     if (index == activeButton) {
         return;
     }
-
-    activeWeight = buttonWeights[activeButton];
+    activeWeight = buttonWeights[index];
     activeButton = index;
 }
 
 /**
  * Momentarily enable the speaker at the pin defined by BUZZER.
- * @param length How long the speaker should be enabled
- * @param wait The time between beeps
+ * @param length How long the speaker should be enabled in milliseconds
+ * @param wait The time between beeps in milliseconds
  * @param amount Amount of times to enable speaker
  */
 void beep(unsigned long length, unsigned long wait, int amount) {
